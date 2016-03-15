2341c132954ca955c0ff5f8864e8e3e312037837
Ethan <yishionwang314@163.com>
2016-03-15 11:36:51 +0800

    newtonsoftjson / regular expression

---
+++ NewtonsoftJson/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NewtonsoftJson
+{
+	class MainClass
+	{
+		public static string json_str = @"{
+										  ""0"": {
+										    ""enable"": true,
+										    ""file_name_sk"": ""Snail_000"",
+										    ""file_name_ui_shell"": ""Snail_000"",
+										    ""file_name_ui_map"": ""MapSnail_000"",
+										    ""file_in_game"": ""Ball_0""
+										  },
+										  ""1"": {
+										    ""enable"": true,
+										    ""file_name_sk"": ""Snail_001"",
+										    ""file_name_ui_shell"": ""Snail_001"",
+										    ""file_name_ui_map"": ""MapSnail_001"",
+										    ""file_in_game"": ""Ball_1""
+										  },
+										  ""2"": {
+										    ""enable"": true,
+										    ""file_name_sk"": ""Snail_002"",
+										    ""file_name_ui_shell"": ""Snail_002"",
+										    ""file_name_ui_map"": ""MapSnail_002"",
+										    ""file_in_game"": ""Ball_2""
+										  },
+										  ""3"": {
+										    ""enable"": false,
+										    ""file_name_sk"": """",
+										    ""file_name_ui_shell"": """",
+										    ""file_name_ui_map"": """",
+										    ""file_in_game"": """"
+										  }
+										}";
+
+		public static void Main (string[] args)
+		{
+			JObject jo = JObject.Parse (json_str);
+			List<bool> jp_list = jo.AsJEnumerable ().Select (jt => {
+				return (JProperty)jt.Value ["enable"].Value<bool> ();
+			}).ToList ();
+
+			Console.WriteLine (JsonConvert.SerializeObject (jp_list, Formatting.Indented));
+		}
+	}
+}
---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace RegularExpressions
+{
+	/// <summary>
+	/// 匹配 office 软件打开文档时所自动生成的临时文件 (excel)
+	/// </summary>
+	class MainClass
+	{
+		public static List<string> test_list = new List<string> {
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/abc~$AEXDataSheet.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/~$AEXDataSheet.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/~$等级表.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/道具相关.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/ab道具相关.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/ab.xlsx",
+			"/Users/EthanW/Documents/UnityProjects/AEX/Submodules/MathLabraryExtendForUnity/DataWorkSheet/a_b.xlsx"
+		};
+
+		public static void Main (string[] args)
+		{
+
+			test_list.ForEach (text => {
+				Match m = Regex.Match (text, "\\/([_a-zA-Z0-9\\u4e00-\\u9fa5]*\\.xlsx)$");
+				Console.WriteLine (text + " / " + m.Groups [1] + " / " + m.Value);
+			});
+		}
+	}
+}
