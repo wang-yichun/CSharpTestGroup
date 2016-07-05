14fbe56e8eee993e3b8103ebf66603a509e281ef
Ethan <yishionwang314@163.com>
2016-07-05 19:23:37 +0800

    up

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace RegularExpressions
 {
@@ -31,28 +32,70 @@ namespace RegularExpressions
 		//		};
 
 		// test 3
-		public static List<string> test = new List<string> {
-			"p0", "p1", "p2", "p 3", "  p4", "p5  ", "###"
+		//public static List<string> test = new List<string> {
+		//	"p0", "p1", "p2", "p 3", "  p4", "p5  ", "###"
+		//};
+
+		// test 4
+		public static IList<string> test = new List<string>
+		{
+			"ftp://192.168.1.1/",
+			"ftp://localhost/",
+			"ftp://192.168.1.1:21/",
+			"ftp://192.168.1.1:21/asset0",
+			"ftp://192.168.1.1:21/pogoframe/asset0",
+			"ftp://192.168.1.1:21/pogoframe/asset0|username",
+			"ftp://192.168.1.1:21/pogoframe/asset0|username|password",
+			"ftp://192.168.1.1:21/pogoframe/asset0|username|password/asset",
 		};
 
-		public static void Main (string[] args)
+		public static void Main(string[] args)
 		{
-// 			test 1
-//			test_list.ForEach (text => {
-//				Match m = Regex.Match (text, "\\/([_a-zA-Z0-9\\u4e00-\\u9fa5]*\\.xlsx)$");
-//				Console.WriteLine (text + " / " + m.Groups [1] + " / " + m.Value);
-//			});
+			// 			test 1
+			//			test_list.ForEach (text => {
+			//				Match m = Regex.Match (text, "\\/([_a-zA-Z0-9\\u4e00-\\u9fa5]*\\.xlsx)$");
+			//				Console.WriteLine (text + " / " + m.Groups [1] + " / " + m.Value);
+			//			});
 
-// 			test 2
-//			test_list_poker.ForEach (text => {
-//				Match m = Regex.Match (text, "^\\(([_A-Z0-9]+),([_A-Z0-9]+)\\)$");
-//				Console.WriteLine (string.Format ("s: {0}, nv: {1}", m.Groups [1].Value, m.Groups [2].Value));
-//			});
+			// 			test 2
+			//			test_list_poker.ForEach (text => {
+			//				Match m = Regex.Match (text, "^\\(([_A-Z0-9]+),([_A-Z0-9]+)\\)$");
+			//				Console.WriteLine (string.Format ("s: {0}, nv: {1}", m.Groups [1].Value, m.Groups [2].Value));
+			//			});
 
-			test.ForEach (text => {
-				Match m = Regex.Match (text, @"[^\d]*(\d+)");
-				Console.WriteLine (string.Format ("suc:{0} 0: {1}, 1: {2}", m.Success, m.Groups [0].Value, m.Groups [1].Value));
-			});
+			//test.ForEach(text =>
+			//{
+			//	Match m = Regex.Match(text, @"[^\d]*(\d+)");
+			//	Console.WriteLine(string.Format("suc:{0} 0: {1}, 1: {2}", m.Success, m.Groups[0].Value, m.Groups[1].Value));
+			//});
+
+			for (int i = 0; i < test.Count; i++)
+			{
+				string str = test[i];
+
+				Match m = Regex.Match(str, @"ftp://([\s\S]*?)(:(\d+))?/([^\|]*)(\|([^\|]+))?(\|([^\|/]+))?");
+				//Console.WriteLine(string.Format(
+				//	"suc:{0}\t0: {1}\t 1: {2},\t 2: {3},\t 3: {4},\t 4: {5},\t 5: {6},\t 6: {7}",
+				//	m.Success,
+				//	m.Groups[0].Value,
+				//	m.Groups[1].Value,
+				//	m.Groups[2].Value,
+				//	m.Groups[3].Value,
+				//	m.Groups[4].Value,
+				//	m.Groups[5].Value,
+				//	m.Groups[6].Value,
+				//	m.Groups[7].Value
+				//));
+
+
+				StringBuilder sb = new StringBuilder();
+				for (int j = 0; j < m.Groups.Count; j++)
+				{
+					sb.AppendFormat("{0} - {1}\n", j, m.Groups[j].Value);
+				}
+				Console.WriteLine(string.Format("str:{0}\nsuc:{1}\ngroups:\n{2}", str, m.Success, sb.ToString()));
+
+			}
 		}
 	}
 }
---
+++ SimpleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SimpleTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Dictionary<string, int> dic = new Dictionary<string, int> ();
+			dic ["a"] = 12;
+			dic ["b"] = 13;
+
+			Console.WriteLine (dic.Count);
+			Console.WriteLine (dic["a"]);
+			Console.WriteLine (dic["c"]);
+		}
+	}
+}
